fd0894c8ec1eb1212ae63c95b47d5bf1e62e2e90
Serdar Salci <serdarsalci@gmail.com>
2021-02-07 21:59:02 -0500

    47 min in part 1

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import useStyles from './styles'
+
+const Post = () => {
+	const classes = useStyles()
+	return (
+		<>
+			<h2>Post</h2>
+		</>
+	)
+}
+
+export default Post
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,19 @@
-export const getPosts = (req, res) => {
-	res.send('this works')
-}
+import PostMessage from '../models/PostMessage.js'
+import asyncHandler from 'express-async-handler'
+
+export const getPosts = asyncHandler(async (req, res) => {
+	const postMessages = await PostMessage.find()
+	res.status(200).json(postMessages)
+})
+
+export const createPost = asyncHandler(async (req, res) => {
+	const post = req.body
+	const newPost = new PostMessage(post)
+	try {
+		await newPost.save()
+		res.status(201).json(newPost)
+	} catch (error) {
+		res.status(409).json({ message: error.message })
+		throw new Error('post not created')
+	}
+})
